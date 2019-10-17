2a7e8d0d2d2d9b0f24c47a562a84e9b1a51c1c3c
EC2 Default User <ec2-user@ip-10-0-0-126.eu-central-1.compute.internal>
2019-10-17 08:04:27 +0000

    Add small test file to g2g

---
+++ test.py
@@ -0,0 +1,9 @@
+import scipy.sparse as sp
+from g2g.model import Graph2Gauss
+from g2g.utils import load_dataset
+
+g = load_dataset('data/cora_ml.npz')
+A, X, z = g['A'], g['X'], g['z']
+
+g2g = Graph2Gauss(A, X, L=64, verbose=True)
+sess = g2g.train()
